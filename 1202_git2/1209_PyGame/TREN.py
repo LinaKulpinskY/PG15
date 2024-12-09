4f06a8ab6d500019d68a4c911e57dd99262b4f49
kulpinskajaea-1 <kulpinskajaea-1@lina>
2024-12-09 17:19:38 +0300

    мышка::

--- 1202_git2/1209_PyGame/TREN.py
+++ 1202_git2/1209_PyGame/TREN.py
@@ -1,6 +1,5 @@
 import pygame
-import  random
-import sys
+import random
 
 # pygame setup
 pygame.init()
@@ -9,6 +8,11 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 f = True
+WHITE = (255, 255, 255)
+RED = (225, 0, 50)
+GREEN = (0, 225, 0)
+BLUE = (0, 0, 225)
+
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 player_pos2 = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -17,30 +21,6 @@ player_pos4 = pygame.Vector2(screen.get_width() / 2 - 30, screen.get_height() / 
 hight = screen.get_height()
 width = screen.get_width()
 while running:
-    pygame = pygame
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            sys.exit()
-        if i.type == pygame.MOUSEBUTTONDOWN:
-            if i.button == 1:
-                pygame.draw.circle(sc, RED, i.pos, 20)
-                pygame.display.update()
-            elif i.button == 3:
-                pygame.draw.circle(sc, BLUE, i.pos, 20)
-                pygame.draw.rect(sc, GREEN,
-                              (i.pos[0] - 10,
-                              i.pos[1] - 10, 20, 20))
-                pygame.display.update()
-            elif i.button == 2:
-                sc.fill(WHITE)
-                pygame.display.update()
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("#09182b")
     for i in range(100):
         screen.fill(pygame.Color('white'),
@@ -51,14 +31,14 @@ while running:
         pygame.draw.circle(screen, "#002238", player_pos3, 20)
         pygame.draw.circle(screen, "#002238", player_pos4, 20)
     elif not f:
-        pygame.draw.rect(screen, "#002238", (player_pos4.x + 43, player_pos4.y - 10, 20 + 20,  20), 0)
-        pygame.draw.rect(screen, "#002238", (player_pos4.x - 23, player_pos4.y - 10, 20 + 20,  20), 0)
+        pygame.draw.rect(screen, "#002238", (player_pos4.x + 43, player_pos4.y - 10, 20 + 20, 20), 0)
+        pygame.draw.rect(screen, "#002238", (player_pos4.x - 23, player_pos4.y - 10, 20 + 20, 20), 0)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
         f = True
         if player_pos.x < 1155:
-            player_pos2.x += 290*dt
+            player_pos2.x += 290 * dt
             player_pos3.x += 280 * dt
             player_pos4.x += 280 * dt
             player_pos.x += 300 * dt
@@ -71,7 +51,7 @@ while running:
             player_pos.x -= 300 * dt
     if keys[pygame.K_w]:
         f = False
-        if player_pos.y < 695:
+        if player_pos.y < 600:
             player_pos2.y -= 290 * dt
             player_pos3.y -= 280 * dt
             player_pos4.y -= 280 * dt
@@ -79,22 +59,29 @@ while running:
             # player_pos5.y -= 280 * dt
     if keys[pygame.K_s]:
         f = False
-        if player_pos.y > 0:
+        if player_pos.y > 20:
             player_pos2.y += 290 * dt
             player_pos3.y += 280 * dt
             player_pos4.y += 280 * dt
             player_pos.y += 300 * dt
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            py_x = pygame.mouse.get_pos()
+            if py_x[0] > (player_pos.x - 30 and py_x[0] < (player_pos.x + 30)):
+                print('peijfpwief')
 
 
+    # fill the screen with a color to wipe away anything from last frame
 
-
-
+    pygame.time.delay(20)
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
+    dt = clock.tick(120) / 1000
--- 1205_PyGame1/main.py
+++ 1205_PyGame1/main.py
@@ -1,32 +1,99 @@
 import pygame
+import  random
+import sys
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+f = True
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos2 = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos3 = pygame.Vector2(screen.get_width() / 2 + 30, screen.get_height() / 2 - 30)
+player_pos4 = pygame.Vector2(screen.get_width() / 2 - 30, screen.get_height() / 2 - 30)
+hight = screen.get_height()
+width = screen.get_width()
+while running:
+    pygame = pygame
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            sys.exit()
+        if i.type == pygame.MOUSEBUTTONDOWN:
+            if i.button == 1:
+                pygame.draw.circle(, RED, i.pos, 20)
+                pygame.display.update()
+            elif i.button == 3:
+                pygame.draw.circle(sc, BLUE, i.pos, 20)
+                pygame.draw.rect(sc, GREEN,
+                                 (i.pos[0] - 10,
+                                  i.pos[1] - 10, 20, 20))
+                pygame.display.update()
+            elif i.button == 2:
+                sc.fill(WHITE)
+                pygame.display.update()
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("#09182b")
+    for i in range(100):
+        screen.fill(pygame.Color('white'),
+                    (random.random() * width, random.random() * hight, 1, 150))
+    pygame.draw.circle(screen, "#ddeeff", player_pos, 120)
+    pygame.draw.circle(screen, "#8db3d9", player_pos2, 100)
+    if f:
+        pygame.draw.circle(screen, "#002238", player_pos3, 20)
+        pygame.draw.circle(screen, "#002238", player_pos4, 20)
+    elif not f:
+        pygame.draw.rect(screen, "#002238", (player_pos4.x + 43, player_pos4.y - 10, 20 + 20,  20), 0)
+        pygame.draw.rect(screen, "#002238", (player_pos4.x - 23, player_pos4.y - 10, 20 + 20,  20), 0)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        f = True
+        if player_pos.x < 1155:
+            player_pos2.x += 290*dt
+            player_pos3.x += 280 * dt
+            player_pos4.x += 280 * dt
+            player_pos.x += 300 * dt
+    if keys[pygame.K_a]:
+        f = True
+        if player_pos.x > 125:
+            player_pos2.x -= 290 * dt
+            player_pos3.x -= 280 * dt
+            player_pos4.x -= 280 * dt
+            player_pos.x -= 300 * dt
+    if keys[pygame.K_w]:
+        f = False
+        if player_pos.y < 695:
+            player_pos2.y -= 290 * dt
+            player_pos3.y -= 280 * dt
+            player_pos4.y -= 280 * dt
+            player_pos.y -= 300 * dt
+            # player_pos5.y -= 280 * dt
+    if keys[pygame.K_s]:
+        f = False
+        if player_pos.y > 0:
+            player_pos2.y += 290 * dt
+            player_pos3.y += 280 * dt
+            player_pos4.y += 280 * dt
+            player_pos.y += 300 * dt
 
 
-def draw(screen):
-    screen.fill((0, 0, 0))
-    font = pygame.font.Font(None, 50)
-    text = font.render("Hello PyGame!", True, (100, 255, 100))
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 2 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (text_x, text_y))
-    pygame.draw.rect(screen, (0, 255, 0),
-                (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
 
 
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 800, 600
-    screen = pygame.display.set_mode(size)
-    # Смена кадров (обновление экрана)
-    draw(screen)
+
+    # flip() the display to put your work on screen
     pygame.display.flip()
-    running = True
-    while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
 
-    pygame.quit()
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
